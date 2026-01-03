f8274559491bfd1db0fe426a8317357db081dfd9
hamassassin <mark@macrho.net>
2026-01-02 20:10:14 -0500

    Update notify.py

--- POTA/notify.py
+++ POTA/notify.py
@@ -1,9 +1,15 @@
 #------------------------------------------------------------------------------------------------#
 # Learning a bit in Python, so expect errors or code that is completely wrong
-# Sends a Pushover when any POTA activators are from a given location
+# Sends a Pushover when any POTA activators that are from a given location
 # Only spots that happened less than 2 minutes ago are included
-# as well as we are restricted to just FT4 and FT8
+# as well as we are restricted to just FT4 and FT8.
+# Three APIs are used:
+#  POTA (Parks on the Air): Free API
+#  QRZ: Have to be a paid Xml subscriber to use
+#  Pushover: Notifications
+#
 # NOTE: You need to create an .env file in the root of your project that contains the variables:
+#       Remember, if in git, add .gitignore that includes .env in it, don't want secrets in git!
 # PUSHOVER_TOKEN
 # PUSHOVER_USER
 # QRZ_USERNAME
@@ -21,7 +27,8 @@ from dotenv import load_dotenv
 import os
 import xml.etree.ElementTree as ET
 
-load_dotenv()  # Load variables from .env file
+# Load variables from .env file, these are our "secrets" for accessing APIs
+load_dotenv()
 
 # Rough conversion from frequency to band, cast the net very wide
 def get_ham_band(value):
@@ -31,7 +38,7 @@ def get_ham_band(value):
   elif frequency >= 3500 and frequency <= 4000:
    return "80m"
   elif frequency >= 5300 and frequency <= 5500:
-     return "60m" # Note: One ham band with channels plus can only transmit a maximum of 100w
+     return "60m"
   elif frequency >= 7000 and frequency <= 7300:
      return "40m"
   elif frequency >= 10100 and frequency <= 10150:
@@ -49,12 +56,11 @@ def get_ham_band(value):
   else:
      return " [ERROR: Not Mapped]: "+ str(frequency)
 
-# Retrieve the QRZ key to use in subsequent API calls
-# The key is good for 24 hours so at some point, cache this and update daily
-# instead of what is being done now
+# Retrieves the QRZ key to use in subsequent API calls
+# The key is good for 24 hours so at some point, cache this and update daily instead of what is being done now
 def get_qrz_key():
   qrz_headers = {'Accept': 'application/xml'}
-  qrz_response = requests.get(url='http://online.qrz.com/bin/xml?username=' + os.getenv('QRZ_USERNAME') + ';password=' + os.getenv('QRZ_PASSWORD'),
+  qrz_response = requests.get(url=f"http://online.qrz.com/bin/xml?username={os.getenv('QRZ_USERNAME')};password={os.getenv('QRZ_PASSWORD')}",
                               headers=qrz_headers)
   root = ET.fromstring(qrz_response.content)
   ns = {"qrz": "http://online.qrz.com"}  # map a prefix to the default NS
@@ -65,10 +71,9 @@ def get_qrz_key():
 # Eventually will get this to return an object
 # NOTE: That is a field is null, it will not be present in the xml
 # https://www.qrz.com/XML/specifications.1.2.html
-def get_qrz_callsign_info(callsign):
+def get_qrz_callsign_info(callsign, qrz_key):
   qrz_headers = {'Accept': 'application/xml'}
-  qrz_key = get_qrz_key() # Grab our key for the API call
-  qrz_url = 'http://online.qrz.com/bin/xml?s='+ qrz_key + ';callsign='+callsign
+  qrz_url = f"http://online.qrz.com/bin/xml?s={qrz_key};callsign={callsign}"
   qrz_resp = requests.get(qrz_url, headers=qrz_headers)
   # Make sure we have a response.. fix this later
   if qrz_resp.text:
@@ -85,12 +90,13 @@ def get_qrz_callsign_info(callsign):
   else:
     return 'Not Found'
 
-# Our POTA API
+# Our POTA API, create a class for this at some point
 response = requests.get("https://api.pota.app/spot/activator")
+
 spots = json.loads(response.text)
 
 # Locations of interest
-locations = ["US-HI", "US-RI", "US-ME", "US-NH", "US-CA", "US-ID", "US-VT", "US-NH"]
+locations = ["US-HI", "US-RI", "US-ME", "US-NH", "US-CA", "US-FL", "US-ID", "US-VT", "US-NH"]
 
 # An array to hold our notification text per spot
 notify = []
@@ -98,29 +104,42 @@ notify = []
 # Current time in utc as is the convention for hams (0 offset)
 now = datetime.now(pytz.utc)
 
+# Grab our key for the QRZ API call, no need to grab it repeatedly
+# Will need to store this key as it's good for 24-hours
+qrz_key = get_qrz_key()
+
 # Over the spots we shall go
 for spot in spots:
+
   # Only interested in digital modes: FT4 or FT8
   if spot["mode"] == "FT8" or spot["mode"] == "FT4":
+
     # Check if it is a location we are interested in
     if spot["locationDesc"] in locations:
+
       # Make sure our datetime objects in UTC
       spot_datetime = datetime.fromisoformat(spot["spotTime"]+'+00:00')
+
+      # And the difference between the time we made the call to the API and the spot was recorded in the API is...
       difference = now - spot_datetime
 
       # We are only interested in spots that occurred less than 2 minutes ago
       if difference.total_seconds() < 120:
-        heard = ' (' + str(difference.seconds) + ' seconds ago)'
+
+        heard = f" ({str(difference.seconds)} seconds ago)"
+
         # Scarf up our signals that were heard and make it somewhat readable for Pushover
-        notify.append('[' + spot["mode"] + ':' + spot["locationDesc"] + '] ' + spot["activator"] + ' (' + get_qrz_callsign_info(spot["activator"]) + ') was at ' + spot["name"] + ' on ' + get_ham_band(spot["frequency"]) + heard)
+        notify.append(f"[{spot["mode"]}:{spot["locationDesc"]}] {spot["activator"]} ({get_qrz_callsign_info(spot["activator"], qrz_key)}) was at {spot["name"]} on {get_ham_band(spot["frequency"])} {heard}")
 
 # Only send a Pushover IF there are elements in the array
 if notify:
+
   # Ignore SSL issues
   ssl._create_default_https_context = ssl._create_unverified_context
 
   # Time to send a pushover out: We'll just join the notify array split by a carriage return
   conn = http.client.HTTPSConnection("api.pushover.net:443")
+
   conn.request("POST",
                    "/1/messages.json",
                        urllib.parse.urlencode({
